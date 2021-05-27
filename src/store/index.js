bfff64fe95c8b7e2f57428b81bba4c33f9d2d399
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2021-05-27 11:17:24 -0300

    configuring reactotron redix-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,22 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import createSagaMiddleware from 'redux-saga';
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./modules/root";
 import rootSaga from "./modules/rootSaga";
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === "development"
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
 //redux and reactotron integration
 const enhancer =
-  process.env.NODE_ENV === 'development'
-    ? compose(
-        console.tron.createEnhancer(),
-        applyMiddleware(sagaMiddleware)
-      )
+  process.env.NODE_ENV === "development"
+    ? compose(console.tron.createEnhancer(), applyMiddleware(sagaMiddleware))
     : applyMiddleware(sagaMiddleware);
 
 const store = createStore(rootReducer, enhancer);
